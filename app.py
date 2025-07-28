20e6022fc966abe675ffd6ec11c4634740f97090
Victor Hugo de Toledo Ruiz <vhtruizz@gmail.com>
2025-07-28 18:14:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import Gerenciador_de_Tarefas
-while True: 
+import Gerenciador_de_Tarefas #importando o outro módulo criado anteriormente
+while True: #utilização de um loop para o programa continuar rodando até atingir a condição de parada
     opcao = input('\nDigite o que você deseja realizar:\n'
                  '1. CRIAR TAREFA\n'
                  '2. LISTAR TAREFAS PENDENTES\n'
@@ -8,15 +8,15 @@ while True:
                  '5. SAIR\n'
                  '\nOpção: ')
     
-    if opcao == '1':
-        Gerenciador_de_Tarefas.criar_tarefa()
+    if opcao == '1': 
+        Gerenciador_de_Tarefas.criar_tarefa() #chama a função criar tarefa do módulo importado
     elif opcao == '2':
-        Gerenciador_de_Tarefas.listar_tarefa()
+        Gerenciador_de_Tarefas.listar_tarefa() #chama a função listar tarefa do módulo importado
     elif opcao == '3':
-        Gerenciador_de_Tarefas.marcar_como_concluida()
+        Gerenciador_de_Tarefas.marcar_como_concluida() #chama a função marcar como concluida do módulo importado
     elif opcao == '4':
-        Gerenciador_de_Tarefas.remove_tarefa()
+        Gerenciador_de_Tarefas.remove_tarefa() #chama a função remove tarefa do módulo importado
     elif opcao == '5':
-        if Gerenciador_de_Tarefas.sair():
-          break
+        if Gerenciador_de_Tarefas.sair():  #chama a função sair para finalizar o programa
+          break #aqui se encerra o loop e consequentemente o programa
         
